4debd1e5554f08fe1eb65c89b075c8b4fd0c0a85
rolltrends <rpmartinez@trends.com.ph>
2025-04-16 10:36:58 +0800

    remove hamburger icon and link to trends and technology

--- client/electron.js
+++ client/electron.js
@@ -1,14 +1,25 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768,
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // preload: path.join(__dirname, 'preload.js'),
+      // contextIsolation: true,
+      // enableRemoteModule: false,
+      nodeIntegration: true,
+    },
   });
 
-  // Load your built React app directly from the file system.
-  mainWindow.loadFile(path.join(__dirname, 'build', 'index.html'));
+  win.loadFile(path.join(__dirname, 'build/index.html'));
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'win32') {
+    app.quit();
+  }
+});
--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -2,15 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import Badge from '@mui/material/Badge';
-import MailIcon from '@mui/icons-material/Mail';
 import { useNavigate, Outlet } from 'react-router-dom';
 import { AuthContext } from './authenContext';
 import axios from 'axios';
@@ -20,16 +14,7 @@ const pages = ['TRENDS & TECHNOLOGIES, INC.'];
 
 function ResponsiveAppBar({ showLogout = true }) {
   const { setUser } = React.useContext(AuthContext);
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
   const navigate = useNavigate();
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   const handlePage = (page) => {
     navigate(`/${page.toLowerCase()}`);
@@ -62,7 +47,7 @@ function ResponsiveAppBar({ showLogout = true }) {
               component="a"
               href="#"
               sx={{
-                display: { xs: 'none', md: 'flex' },
+                display: 'flex',
                 mr: 2,
               }}
             >
@@ -74,56 +59,7 @@ function ResponsiveAppBar({ showLogout = true }) {
               />
             </Box>
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{ display: { xs: 'block', md: 'none' } }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={() => handlePage(page)}>
-                    <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
-              }}
-            >
-              <img
-                src="/Trends-logo-with-Tagline-transparent-2048x599.png" // Reference the logo in the public folder
-                alt="Trends Logo"
-                style={{ height: '40px', cursor: 'pointer' }}
-                onClick={() => navigate('/')}
-              />
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Box sx={{ flexGrow: 1, display: 'flex' }}>
               {pages.map((page) => (
                 <Button
                   key={page}
